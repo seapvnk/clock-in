b6a996925249127874fb5d29c3575887e297bfcc
Pedro <www.psro@gmail.com>
2020-09-18 15:11:53 -0300

    Database: query method created

--- src/config/Database.php
+++ src/config/Database.php
@@ -14,4 +14,12 @@ class Database
 
         return $conn;
     }
+
+    public static function query($sql)
+    {
+        $conn = self::getConnection();
+        $result = $conn->query($sql);
+        $conn->close();
+        return $result;
+    }
 }
