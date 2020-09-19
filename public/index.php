1e1e1647174b70758eee6c5fca8cca61249cbc86
Pedro <www.psro@gmail.com>
2020-09-19 15:54:55 -0300

    App: Using new view

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once dirname(__FILE__, 2) . '/src/config/config.php';
+require_once dirname(__FILE__, 2) . '/src/views/View.php';
+require_once dirname(__FILE__, 2) . '/src/views/Login.php';
+
+
+Login::render();
--- src/config/config.php
+++ src/config/config.php
@@ -4,6 +4,8 @@ date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 
 // Directories
+define('APP_NAME', 'Clock-in!');
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
+define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
 
 require_once realpath(dirname(__FILE__) . '/Database.php');
