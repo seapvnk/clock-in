0ee580099011581d0319f7d54b08d309e823df41
Pedro <www.psro@gmail.com>
2020-09-19 14:24:58 -0300

    Model: Select method

--- src/models/Model.php
+++ src/models/Model.php
@@ -11,15 +11,6 @@ class Model
         $this->load($arr);
     }
 
-    public function load($arr)
-    {
-        if ($arr) {
-            foreach ($arr as $key => $value) {
-                $this->$key=  $value;
-            }
-        }
-    }
-
     public function __get($key)
     {
         return $this->values[$key];
@@ -29,4 +20,46 @@ class Model
     {
         $this->values[$key] = $value;
     }
+
+    public function load($arr)
+    {
+        if ($arr) {
+            foreach ($arr as $key => $value) {
+                $this->$key=  $value;
+            }
+        }
+    }
+
+    public static function select($filters = [], $columns = '*')
+    {
+        $sql = "SELECT $columns FROM "
+                . static::$table
+                . ;
+        return $sql;
+    }
+
+    private static function filters($filters)
+    {
+        $sql = '';
+        if (count($filters) > 0) {
+            $sql .= "WHERE 1 = 1";
+
+            foreach($filters as $column => $value) {
+                $sql .= " AND ${column} = " . static::getFormatedValue($value);
+            }
+        }
+        return $sql;
+    }
+
+    private static function getFormatedValue($value)
+    {
+        if (is_null($value)) {
+            return "null";
+        } elseif (is_string($value)) {
+            return "'${value}'";
+        }
+        
+        return $value;
+    }
+
 }
