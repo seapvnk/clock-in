4da09d09bb7147fda2b0f9568bc0a21ba69ff7fb
Pedro Sérgio <www.psro@gmail.com>
2020-09-20 12:01:12 -0300

    Templates: base

--- src/views/templates/base.php
+++ src/views/templates/base.php
@@ -14,7 +14,7 @@
 
     <!-- CSS Files -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="assets/css/now-ui-kit.css" rel="stylesheet" />
+    <link href="assets/css/now-ui-kit.min.css" rel="stylesheet" />
 
     <link rel="stylesheet" href="assets/css/styles.css">
 
