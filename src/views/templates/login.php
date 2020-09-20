a23defeaeb2b344ae6b5fd087cf538e14750553a
Pedro Sérgio <www.psro@gmail.com>
2020-09-20 12:01:43 -0300

    View: Login form

--- src/views/templates/login.php
+++ src/views/templates/login.php
@@ -1,15 +1,17 @@
-<div class="container">
+<div class="container login-container">
     <form action="" method="post">
         <div class="card login-card">
         
-            <div class="card-header">
-                <h1 class="text-center">
-                    <i class="fas fa-globe"></i>
+            <div class="card-header bg-primary text-white">
+                <h1 class="text-center" style="padding: 0.1em 0">
+                    <i class="fas fa-clock"></i>
                     clock-in!
                 </h1>
             </div>
 
+    
             <div class="card-body">
+
                 <div class="form-group">
                     <label for="email">E-mail</label>
                     <input
@@ -17,10 +19,26 @@
                         type="email" 
                         name="email" 
                         id="email"
-                        placeholder="Seu e-mail"
+                        placeholder="Informe seu e-mail"
                         autofocus
                     >
                 </div>
+
+                <div class="form-group">
+                    <label for="password">Senha</label>
+                    <input
+                        class="form-control"
+                        type="password" 
+                        name="password" 
+                        id="password"
+                        placeholder="Informe sua senha"
+                        autofocus
+                    >
+                </div>
+            </div>
+
+            <div class="card-footer" style="padding: 1rem">
+                <button class="btn btn-lg btn-primary">Entrar</button>
             </div>
         
         </div>
