8ee8f45606a6538f29fa16065c7fdf2e3119a892
Pedro Sérgio <www.psro@gmail.com>
2020-09-20 14:03:22 -0300

    View: receiving parameters

--- src/views/View.php
+++ src/views/View.php
@@ -5,13 +5,23 @@ class View
     protected static $styles = [];
     protected static $templates = [];
 
-    public static function render()
+    public static function render($params = [])
     {
 
         $currentView = get_called_class();
 
         $styles = $currentView::$styles;
         $templates = $currentView::$templates;
+
+        // initialize variables used in templates
+        if (count($params) > 0) {
+            foreach ($params as  $variableName => $value) {
+                if (strlen($variableName) > 0) {
+                    ${$variableName} = $value;
+                }
+            }
+        }
+
         require_once VIEW_PATH . "/templates/base.php";
     }
 
