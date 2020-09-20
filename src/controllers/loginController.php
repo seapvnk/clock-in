610b10811578fad72f9e167d4a41e384104e1ca7
Pedro Sérgio <www.psro@gmail.com>
2020-09-20 14:06:18 -0300

    Controllers added

---
+++ src/controllers/loginController.php
@@ -0,0 +1,20 @@
+<?php
+
+function loginController() {
+
+    loadModel('Login');
+    
+    if (count($_POST) > 0) {
+        $login = new Login($_POST);
+
+        try {
+            $user = $login->checkLogin();
+            echo "Usuário {$user->name} logado!";
+        } catch(Exception $e) {
+            echo "Falha no login";
+        }
+    }
+    
+    loadView('LoginView', $_POST);
+
+}
