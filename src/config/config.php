de7dd7972f88f433832075e8990ae5a5e30903fc
Pedro Sérgio <www.psro@gmail.com>
2020-09-20 12:25:05 -0300

    Config: loading classes reference

--- src/config/config.php
+++ src/config/config.php
@@ -8,4 +8,8 @@ define('APP_NAME', 'Clock-in!');
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
 define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
 
+
+// Classes
 require_once realpath(dirname(__FILE__) . '/Database.php');
+require_once realpath(MODEL_PATH . '/Model.php');
+require_once realpath(VIEW_PATH . '/View.php');
--- src/views/Login.php
+++ src/views/Login.php
@@ -1,13 +1,10 @@
 <?php
 
-require_once VIEW_PATH . '/View.php';
-
 class Login extends View
 {
     protected static $styles = ['login'];
     protected static $templates = ['login'];
 
 }
-?>
 
 
