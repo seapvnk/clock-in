646adee1b9dfddd1b5c6eda35afb2351f6b2fae6
Pedro Sérgio <www.psro@gmail.com>
2020-09-21 06:18:01 -0300

    Templates: message created

---
+++ src/views/templates/messages.php
@@ -0,0 +1,34 @@
+<?php
+
+$errors = [];
+$message = null;
+if ($exception) {
+    $message = [
+        'type' => 'error',
+        'message' => $exception->getMessage(),
+    ];
+
+    if (get_class($exception) === 'ValidationException') {
+        $errors = $exception->getErrors();  
+    }
+}
+
+
+
+
+$alertType = 'info';
+if ($message && $message['type'] === 'error') {
+    $alertType = 'danger';
+}
+
+
+?>
+
+<?php if ($message): ?>
+    <div
+        class="alert alert-<?= $alertType ?>" 
+        role="alert"
+    >
+        <?= $message['message'] ?>
+    </div>
+<?php endif; ?>
