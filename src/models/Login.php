e1ec8510b3d9bad2b332cf39fe50741c0717a50c
Pedro Sérgio <www.psro@gmail.com>
2020-09-21 06:19:32 -0300

    Login: validate multiple fields

--- src/models/Login.php
+++ src/models/Login.php
@@ -3,16 +3,41 @@
 loadModel('User');
 
 class Login extends Model {
+
+    public function validate()
+    {
+        $errors = [];
+        
+        if (!$this->email) {
+            $errors['email'] = 'E-mail é um campo obrigatório.';
+        }
+
+        if (!$this->password) {
+            $errors['password'] = 'Por favor, insira a senha.';
+        }
+
+        if (count($errors) > 0) {
+            throw new ValidationException($errors);
+        }
+
+    }
     
     public function checkLogin()
     {
+        $this->validate();
         $user = User::one(['email' => $this->email]);
         if ($user) {
+
+            if ($user->end_date) {
+                throw new AppException('O usuário não faz mais parte da empresa.');
+            }
+
             if (password_verify($this->password, $user->password)) {
                 return $user;
             }
+
         }
 
-        throw new Exception();
+        throw new AppException('Usuário e senha inválidos.');
     }
 }
