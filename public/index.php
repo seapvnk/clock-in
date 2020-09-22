0afc96cc5fcc243011c28a8f168340c6673a3191
Pedro Sérgio <www.psro@gmail.com>
2020-09-22 16:14:40 -0300

    Public: handle routes

--- public/index.php
+++ public/index.php
@@ -1,6 +1,16 @@
 <?php
 
 require_once dirname(__FILE__, 2) . '/src/config/config.php';
-require_once CONTROLLER_PATH . '/loginController.php';
 
-loginController();
+$uri = str_replace(BASE_ADDR, '', urldecode($_SERVER['REQUEST_URI']));
+
+if ($uri === '/' || $uri === '' || $uri === 'index.php') {
+    $uri = 'login';
+}
+
+if ($uri === 'day') {
+    $uri = 'day';
+}
+
+Loader::controller($uri);
+
