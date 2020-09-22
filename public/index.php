e5b21160b85eac51afafd3f9fcd8d18e0c7a35c1
Pedro Sérgio <www.psro@gmail.com>
2020-09-22 17:13:27 -0300

    Routes: parse route

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require_once dirname(__FILE__, 2) . '/src/config/config.php';
 
 $uri = str_replace(BASE_ADDR, '', urldecode($_SERVER['REQUEST_URI']));
+$uri = parse_url($uri, PHP_URL_PATH);
 
 if ($uri === '/' || $uri === '' || $uri === 'index.php') {
     $uri = 'login';
