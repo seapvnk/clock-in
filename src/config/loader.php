93104439b90dd2915edfa21e21db40165d6a099e
Pedro Sérgio <www.psro@gmail.com>
2020-09-22 11:20:33 -0300

    Loader: loadController

--- src/config/loader.php
+++ src/config/loader.php
@@ -10,3 +10,10 @@ function loadView($name, $params = [])
     require_once VIEW_PATH . "/{$name}.php";
     $name::render($params);
 }
+
+function loadController($name)
+{
+    $controller = "{$name}Controller";
+    require_once CONTROLLER_PATH ."/{$controller}.php";
+    call_user_func($controller);
+}
