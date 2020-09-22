92b5dfc0458125e05b4c33249331e5c6d383316d
Pedro Sérgio <www.psro@gmail.com>
2020-09-22 11:24:21 -0300

    Loader: loader as class

---
+++ src/config/Loader.php
@@ -0,0 +1,24 @@
+<?php
+
+class Loader
+{
+    public static function model($name)
+    {
+        require_once MODEL_PATH . "/{$name}.php";
+    }
+    
+    public static function view($name, $params = [])
+    {
+        require_once VIEW_PATH . "/{$name}.php";
+        $name::render($params);
+    }
+    
+    public static function controller($name)
+    {
+        $controller = "{$name}Controller";
+        require_once CONTROLLER_PATH ."/{$controller}.php";
+        call_user_func($controller);
+    }
+}
+
+
