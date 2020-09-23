c18b28b39022c19d8606d873149ad33d300cb9af
Pedro Sérgio <www.psro@gmail.com>
2020-09-23 11:45:30 -0300

    Templates: footer

---
+++ src/views/templates/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+
+    <span>Desenvolvido com </span>
+    <span class="text-primary"><i class="fas fa-heart mx-1"></i></span>
+    <span> por <a href="https://github.com/seapvnk" target="_blank" rel="noopener noreferrer">Pedro Sérgio</a></span>
+
+</footer>
+
+
+<script src="assets/js/app.js" ></script>
