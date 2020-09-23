b689e2df701ec86086be9aa96e5f935b037e30d8
Pedro Sérgio <www.psro@gmail.com>
2020-09-23 11:45:50 -0300

    Templates: left(aside)

---
+++ src/views/templates/left.php
@@ -0,0 +1,18 @@
+<aside class="sidebar">
+    <nav class="menu mt-3">
+        <ul class="nav-list">
+            <li class="nav-item">
+                <a href="/day"><i class="fas fa-check mr-2"></i> Registrar ponto</a>
+            </li>
+            <li class="nav-item">
+                <a href=""><i class="far fa-calendar mr-2"></i> Relatório mensal</a>
+            </li>
+            <li class="nav-item">
+                <a href=""><i class="fas fa-chart-bar mr-2"></i> Relatório gerencial</a>
+            </li>
+            <li class="nav-item">
+                <a href=""><i class="fas fa-users mr-2"></i> Usuários</a>
+            </li>
+        </ul>
+    </nav>
+</aside>
