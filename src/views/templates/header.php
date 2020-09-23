223abe6001022d65aef523a0f9b1e4f4e009dc16
Pedro Sérgio <www.psro@gmail.com>
2020-09-23 11:45:39 -0300

    Templates: header

---
+++ src/views/templates/header.php
@@ -0,0 +1,24 @@
+<header class="header">
+    <div class="logo">
+        <i class="fas fa-clock"></i>
+        Clock-in!
+    </div>
+    <div class="menu-toggle mx-3">
+        <i class="fas fa-bars"></i>
+    </div>
+    
+    <div class="spacer"></div>
+    <div class="dropdown">
+        <div class="dropdown-button">
+            <span class="ml-2">Usuário Mock</span>
+            <i class="fas fa-chevron-down mx-2"></i>
+        </div>
+        <div class="dropdown-content">
+            <ul class="nav-list">
+                <li class="nav-item">
+                    <a href="logout"><i class="fas fa-sign-out-alt mr-2"></i>Sair</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
