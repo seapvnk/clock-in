bfb9b106bfba87f9305fb8a1c10955855ec48e07
Pedro Sérgio <www.psro@gmail.com>
2020-09-24 11:25:40 -0300

    Templates: day updated

--- src/views/templates/day.php
+++ src/views/templates/day.php
@@ -1,3 +1,31 @@
-<main class="content">
-    <h1>Ok!</h1>
+<main class="content shadow-sm">
+    <div class="content-title mb-4">
+        <i class="fas fa-check mr-2"></i>
+        <div>
+            <h1>Registrar ponto</h1>
+            <h2>Mantenha seu ponto consistente</h2>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header p-2">
+            <h3 class="text-center">10 de janeiro de 2002</h3>
+            <p class="mb-0 text-center">Os batimentos efetuados hoje</p>
+        </div>
+        <div class="card-body">
+            <div class="d-flex m-4 justify-content-around">
+                <span class="record">Entrada 1: ---</span>
+                <span class="record">Saída 1: ---</span>
+            </div>
+            <div class="d-flex m-4 justify-content-around">
+                <span class="record">Entrada 2: ---</span>
+                <span class="record">Saída 2: ---</span>
+            </div>
+        </div>
+        <div class="card-footer bg-mute d-flex justify-content-end">
+            <a href="?" class="btn btn-success btn-lg m-2">
+                <i class="fas fa-check mr-2"></i>
+                Bater o ponto
+            </a>
+        </div>
+    </div>
 </main>
