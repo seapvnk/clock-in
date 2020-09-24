8ba26ea147472c9da35b74da6373c9fab62ca181
Pedro Sérgio <www.psro@gmail.com>
2020-09-24 12:42:52 -0300

    Day: loading user from session in view

--- src/views/DayView.php
+++ src/views/DayView.php
@@ -5,6 +5,11 @@ class DayView extends View
     protected static $styles = [];
     protected static $templates = ['header', 'left', 'day', 'footer'];
 
+    public static function render($params = [])
+    {
+        parent::render($params + ['user' => unserialize(Session::state()->user)]);
+    }
+
 }
 
 
