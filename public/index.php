9e42f389ca61682f941c164edef3a12ec6d60a97
Pedro Sérgio <www.psro@gmail.com>
2020-09-24 11:52:44 -0300

    Little update in layout & routes

--- public/index.php
+++ public/index.php
@@ -5,12 +5,16 @@ require_once dirname(__FILE__, 2) . '/src/config/config.php';
 $uri = str_replace(BASE_ADDR, '', urldecode($_SERVER['REQUEST_URI']));
 $uri = parse_url($uri, PHP_URL_PATH);
 
-if ($uri === '/' || $uri === '' || $uri === 'index.php') {
-    $uri = 'login';
+if ($uri === '/' || $uri === '') {
+    $uri = 'day';
 }
 
 if ($uri === 'day') {
     $uri = 'day';
+}
+
+if ($uri === 'login') {
+    $uri = 'login';
 }
 
 Loader::controller($uri);
