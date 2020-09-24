dcee2ee5287a85e9edf43c5525b5fbbc3c109c70
Pedro Sérgio <www.psro@gmail.com>
2020-09-24 12:41:09 -0300

    Session: updated

--- src/config/Session.php
+++ src/config/Session.php
@@ -1,6 +1,22 @@
 <?php
 
 class Session {
+
+    public static function state()
+    {
+        return new Session;
+    }
+
+    public function __set($name, $value) 
+    {
+        $_SESSION[$name] = $value;
+    }
+ 
+    public function __get($name) 
+    {
+        return $_SESSION[$name]?? null;
+    }
+
     public static function validate()
     {
         $user = $_SESSION['user']?? null;
