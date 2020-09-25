fb2414317ae39446977e6cbd18ac881c40192df4
Pedro Sérgio <www.psro@gmail.com>
2020-09-25 12:13:09 -0300

    Utility: time consts

--- src/utils/Utility.php
+++ src/utils/Utility.php
@@ -1,6 +1,9 @@
 <?php
 class Utility
 {
+    const DAILY_TIME = 8 * 60 ** 2;
+    const HOUR = 3600;
+    
     public static function redirect($location)
     {
         $url = BASE_URL . '/' . $location;
