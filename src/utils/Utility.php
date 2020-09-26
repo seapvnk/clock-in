663da419d03eddcae18aea3b38edc360db477996
Pedro Sérgio <www.psro@gmail.com>
2020-09-26 17:55:54 -0300

    Utility: Date utilities

--- src/utils/Utility.php
+++ src/utils/Utility.php
@@ -9,4 +9,31 @@ class Utility
         $url = BASE_URL . '/' . $location;
         header("Location: ${url}");
     }
+
+    public static function getDateAsDateTime($date)
+    {
+        return is_string($date)? new Datetime($date) : $date;
+    }
+
+    public static function isWeekend($date)
+    {
+        $inputDate = self::getDateAsDateTime($date);
+        return $inputDate->format('N') >= 6;
+    }
+
+    public static function isBefore($date1, $date2)
+    {
+        $inputDate1 = self::getDateAsDateTime($date1);
+        $inputDate2 = self::getDateAsDateTime($date2);
+
+        return $inputDate1 <= $inputDate2;
+    }
+
+    public static function getNextDay($date)
+    {
+        $inputDate = self::getDateAsDateTime($date);
+        $inputDate->modify('+1 day');
+
+        return $inputDate;
+    }
 }
