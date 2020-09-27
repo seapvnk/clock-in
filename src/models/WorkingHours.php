2397b325768d5842812685a7ed94f2dc535bf655
Pedro Sérgio <www.psro@gmail.com>
2020-09-27 19:20:01 -0300

    WorkingHours: update

--- src/models/WorkingHours.php
+++ src/models/WorkingHours.php
@@ -41,62 +41,73 @@ class WorkingHours extends Model
         return null;
     }
 
+    public function getActiveClock()
+    {
+        $nextTime = $this->getNextTime();
+        switch ($nextTime) {
+            case 'time1':
+            case 'time3':
+                return 'exitTime';
+            case 'time2':
+            case 'time4':
+                return 'workedInterval';
+            default:
+                return null;
+        }
+    }
+
     public function clockin($time)
     {
         $timeColumn = $this->getNextTime();
-
-        if (!$timeColumn) {
-            throw new AppException("Você já realizou os 4 batimentos do dia");
+        if(!$timeColumn) {
+            throw new AppException("Você já realizou os 4 batimentos do dia!");
         }
-
         $this->$timeColumn = $time;
-        
-        if ($this->id) {
+        $this->worked_time = Utility::getSecondsFromDateInterval($this->getWorkedInterval());
+        if($this->id) {
             $this->update();
         } else {
             $this->insert();
         }
     }
 
-    public function getWorkedInterval()
+    function getWorkedInterval()
     {
         [$t1, $t2, $t3, $t4] = $this->getTimes();
 
         $part1 = new DateInterval('PT0S');
         $part2 = new DateInterval('PT0S');
 
-        if ($t1) $part1 = $t1->diff(new DateTime());
-        if ($t2) $part1 = $t1->diff($t2);
-        
-        if ($t3) $part2 = $t3->diff(new DateTime());
-        if ($t4) $part2 = $t4->diff($t4);
-        
-        return Utility::sumIntervals($part1, $part2);
+        if($t1) $part1 = $t1->diff(new DateTime());
+        if($t2) $part1 = $t1->diff($t2);
+        if($t3) $part2 = $t3->diff(new DateTime());
+        if($t4) $part2 = $t3->diff($t4);
 
+        return Utility::sumIntervals($part1, $part2);
     }
 
-    public function getLunchInterval()
+    function getLunchInterval()
     {
-        [, $t2, $t3, ] = $this->getTimes();
+        [, $t2, $t3,] = $this->getTimes();
         $lunchInterval = new DateInterval('PT0S');
 
-        if ($t2) $lunchInterval = $t2->diff(new DateTime());
-        if ($t3) $lunchInterval = $t2->diff($t3);
+        if($t2) $lunchInterval = $t2->diff(new DateTime());
+        if($t3) $lunchInterval = $t2->diff($t3);
 
         return $lunchInterval;
     }
 
-    public function getExitTime()
+    function getExitTime()
     {
-        [$t1, , , $t4] = $this->getTimes();
-        $workDay = DateInterval::createFromDateString('8 hours');
+        [$t1,,, $t4] = $this->getTimes();
+        $workday = DateInterval::createFromDateString('8 hours');
 
-        if (!$t1) {
-            return (new DateTimeImmutable())->add($workDay);
-        } else if (!$t4) {
+        if(!$t1) {
+            return (new DateTimeImmutable())->add($workday);
+        } elseif($t4) {
             return $t4;
         } else {
-            $total = Utility::sumIntervals($workDay, $this->getLunchInterval());
+            $total = Utility::sumIntervals($workday, $this->getLunchInterval());
             return $t1->add($total);
         }
     }
