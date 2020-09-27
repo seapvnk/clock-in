ae93e662702c319d4a2dc3ac27dd9e982af14732
Pedro Sérgio <www.psro@gmail.com>
2020-09-27 19:29:25 -0300

    Templates: receiving data from backend

--- src/views/templates/left.php
+++ src/views/templates/left.php
@@ -2,7 +2,7 @@
     <nav class="menu mt-3">
         <ul class="nav-list">
             <li class="nav-item">
-                <a href="/day"><i class="fas fa-check mr-2"></i> Registrar ponto</a>
+                <a href="day"><i class="fas fa-check mr-2"></i> Registrar ponto</a>
             </li>
             <li class="nav-item">
                 <a href=""><i class="far fa-calendar mr-2"></i> Relatório mensal</a>
@@ -19,7 +19,9 @@
         <div class="sidebar-widget">
             <i class="fas fa-clock icon text-info"></i>
             <div class="info">
-                <span class="main text-info">04:23</span>
+                <span class="main text-info" 
+                    <?= $activeClock === 'workedInterval'? 'active-clock' : '' ?>
+                ><?= $workedInterval ?></span>
                 <span class="label text-mute">Horas trabalhadas</span>
             </div>
         </div>
@@ -29,7 +31,9 @@
         <div class="sidebar-widget">
             <i class="fas fa-stopwatch icon text-danger"></i>
             <div class="info">
-                <span class="main text-danger">18:00</span>
+            <span class="main text-danger" 
+                    <?= $activeClock === 'exitTime'? 'active-clock' : '' ?>
+                ><?= $exitTime ?></span>
                 <span class="label text-mute">Hora de saída</span>
             </div>
         </div>
