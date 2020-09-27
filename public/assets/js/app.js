1dcd87bd2258adc02b74e1a901706d4b64e9b1d0
Pedro Sérgio <www.psro@gmail.com>
2020-09-27 19:29:54 -0300

    App.js: front-end updating time

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -9,3 +9,29 @@
     });
 
 })();
+
+
+(() => {
+
+    const addOneSecond = (hours, minutes, seconds) => {
+        const d = new Date();
+        d.setHours(parseInt(hours));
+        d.setMinutes(parseInt(minutes));
+        d.setSeconds(parseInt(seconds) + 1);
+
+        const h = `${d.getHours()}`.padStart(2, 0);
+        const m = `${d.getMinutes()}`.padStart(2, 0);
+        const s = `${d.getSeconds()}`.padStart(2, 0);
+
+        return `${h}:${m}:${s}`;
+    }
+
+    const clock = document.querySelector('[active-clock]');
+    if (!clock) return;
+
+    setInterval(() => {
+        const parts = clock.innerHTML.split(':');
+        clock.innerHTML = addOneSecond(...parts);    
+    }, 1000);
+    
+})()
