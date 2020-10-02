7b9c5673b45c9bea1c2b59b471c43b73ef92c2ef
Pedro Sérgio <www.psro@gmail.com>
2020-10-02 10:40:38 -0300

    confirmDeleteController: formating date

--- src/controllers/confirmDeleteController.php
+++ src/controllers/confirmDeleteController.php
@@ -9,7 +9,14 @@ function confirmDeleteController()
     }
 
     $userToDelete = User::one(['id' => $_GET['delete']]);
-    
+
+    // Format date
+    $startDate = DateTime::createFromFormat('Y-m-d', $userToDelete->start_date)->format('d/m/Y');
+    $endDate = DateTime::createFromFormat('Y-m-d', $userToDelete->end_date)->format('d/m/Y');
+
+    $userToDelete->start_date = $startDate;
+    $userToDelete->end_date = $endDate;
+
     Loader::view('ConfirmDelete', [
         'userToDelete' => $userToDelete,
     ]);
